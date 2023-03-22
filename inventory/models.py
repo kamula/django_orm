63b3d2c132855caf64aae177ccf65720c59c08ce
kamula <kamulaisaac@gmail.com>
2023-03-22 16:09:14 +0300

    first commit

---
+++ inventory/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    qty = models.IntegerField()
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
